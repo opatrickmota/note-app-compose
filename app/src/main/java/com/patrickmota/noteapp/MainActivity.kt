47a25aac5205c6ccf8b21eec019dba0eb84177a7
Patrick Mota <patrickvieiramota@gmail.com>
2022-10-29 16:59:01 -0300

    FEAT: Adiciona campo de texto e botao

--- app/src/main/java/com/patrickmota/noteapp/MainActivity.kt
+++ app/src/main/java/com/patrickmota/noteapp/MainActivity.kt
@@ -6,12 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.patrickmota.noteapp.screen.NoteScreen
 import com.patrickmota.noteapp.ui.theme.NoteAppTheme
 
+@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,22 +22,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    NoteScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NoteAppTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/patrickmota/noteapp/screen/NoteScreen.kt
+++ app/src/main/java/com/patrickmota/noteapp/screen/NoteScreen.kt
@@ -9,6 +9,10 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Notifications
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -17,11 +21,21 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.patrickmota.noteapp.R
+import com.patrickmota.noteapp.components.NoteButton
 import com.patrickmota.noteapp.components.NoteInputText
 
 @ExperimentalComposeUiApi
 @Composable
 fun NoteScreen() {
+
+    var title by remember {
+        mutableStateOf("")
+    }
+
+    var description by remember {
+        mutableStateOf("")
+    }
+
     Column(modifier = Modifier.padding(6.dp)) {
         TopAppBar(title = {
             Text(text = stringResource(id = R.string.app_name))
@@ -33,7 +47,28 @@ fun NoteScreen() {
             modifier = Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            NoteInputText(text = "hello", label = "Hello", onTextChange = {})
+
+            NoteInputText(
+                modifier = Modifier.padding(top = 9.dp, bottom = 8.dp),
+                text = title,
+                label = "Title",
+                onTextChange = {
+                    if (it.all { char -> char.isLetter() || char.isWhitespace() }) {
+                        title = it
+                    }
+                })
+
+            NoteInputText(
+                modifier = Modifier.padding(top = 9.dp, bottom = 8.dp),
+                text = description,
+                label = "Add a note",
+                onTextChange = {
+                    if (it.all { char -> char.isLetter() || char.isWhitespace() }) {
+                        description = it
+                    }
+                })
+
+            NoteButton(text = "Save", onClick = { /*TODO*/ })
         }
     }
 }
