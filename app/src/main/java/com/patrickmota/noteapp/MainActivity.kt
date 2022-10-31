14be1456d87d093c6b6650826e8385be3c9ba77b
Patrick Mota <patrickvieiramota@gmail.com>
2022-10-31 20:40:47 -0300

    FEAT: Adiciona camada de viewmodel

--- app/src/main/java/com/patrickmota/noteapp/MainActivity.kt
+++ app/src/main/java/com/patrickmota/noteapp/MainActivity.kt
@@ -3,17 +3,19 @@ package com.patrickmota.noteapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import com.patrickmota.noteapp.data.NotesDataSource
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.patrickmota.noteapp.model.Note
 import com.patrickmota.noteapp.screen.NoteScreen
+import com.patrickmota.noteapp.screen.NoteViewModel
 import com.patrickmota.noteapp.ui.theme.NoteAppTheme
 
 @ExperimentalComposeUiApi
@@ -22,24 +24,29 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NoteAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    val notes = remember {
-                        mutableStateListOf<Note>()
-                    }
-                    NoteScreen(
-                        notes = notes,
-                        onAddNote = {
-                            notes.add(it)
-                        },
-                        onRemoveNote = {
-                            notes.remove(it)
-                        })
+                    val noteViewModel: NoteViewModel by viewModels()
+                    NotesApp(noteViewModel = noteViewModel)
                 }
             }
         }
     }
+}
+
+@ExperimentalComposeUiApi
+@Composable
+fun NotesApp(noteViewModel: NoteViewModel = viewModel()) {
+    val notesList = noteViewModel.getAllNotes()
+
+    NoteScreen(
+        notes = notesList,
+        onAddNote = {
+            noteViewModel.addNote(it)
+        },
+        onRemoveNote = {
+            noteViewModel.removeNote(it)
+        })
 }
