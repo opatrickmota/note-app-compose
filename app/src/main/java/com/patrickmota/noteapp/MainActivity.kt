32f5ffad047e49e23f1d1ddf3484335c72316edf
Patrick Mota <patrickvieiramota@gmail.com>
2022-10-31 20:03:32 -0300

    FEAT: Adiciona anotacoes falsa e as estiliza

--- app/src/main/java/com/patrickmota/noteapp/MainActivity.kt
+++ app/src/main/java/com/patrickmota/noteapp/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import com.patrickmota.noteapp.data.NotesDataSource
 import com.patrickmota.noteapp.screen.NoteScreen
 import com.patrickmota.noteapp.ui.theme.NoteAppTheme
 
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    NoteScreen()
+                    NoteScreen(notes = NotesDataSource().loadNotes(), onAddNote = {}, onRemoveNote = {})
                 }
             }
         }
--- app/src/main/java/com/patrickmota/noteapp/screen/NoteScreen.kt
+++ app/src/main/java/com/patrickmota/noteapp/screen/NoteScreen.kt
@@ -1,9 +1,16 @@
 package com.patrickmota.noteapp.screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
@@ -16,6 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,10 +31,17 @@ import androidx.compose.ui.unit.dp
 import com.patrickmota.noteapp.R
 import com.patrickmota.noteapp.components.NoteButton
 import com.patrickmota.noteapp.components.NoteInputText
+import com.patrickmota.noteapp.data.NotesDataSource
+import com.patrickmota.noteapp.model.Note
+import java.time.format.DateTimeFormatter
 
 @ExperimentalComposeUiApi
 @Composable
-fun NoteScreen() {
+fun NoteScreen(
+    notes: List<Note>,
+    onAddNote: (Note) -> Unit,
+    onRemoveNote: (Note) -> Unit
+) {
 
     var title by remember {
         mutableStateOf("")
@@ -68,7 +83,46 @@ fun NoteScreen() {
                     }
                 })
 
-            NoteButton(text = "Save", onClick = { /*TODO*/ })
+            NoteButton(text = "Save", onClick = {
+                if (title.isNotEmpty() && description.isNotEmpty()) {
+                    title = ""
+                    description = ""
+                }
+            })
+        }
+
+        Divider(modifier = Modifier.padding(10.dp))
+
+        LazyColumn {
+            items(notes) { note ->
+                NoteRow(note = note, onNoteCliked = {})
+            }
+        }
+    }
+}
+
+@Composable
+fun NoteRow(modifier: Modifier = Modifier, note: Note, onNoteCliked: (Note) -> Unit) {
+    Surface(
+        modifier
+            .padding(4.dp)
+            .clip(RoundedCornerShape(topEnd = 33.dp, bottomStart = 33.dp))
+            .fillMaxWidth(),
+        color = Color(0xFFDFE6EB),
+        elevation = 6.dp
+    ) {
+        Column(
+            modifier
+                .clickable { }
+                .padding(horizontal = 14.dp, vertical = 6.dp),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(text = note.title, style = MaterialTheme.typography.subtitle2)
+            Text(text = note.description, style = MaterialTheme.typography.subtitle1)
+            Text(
+                text = note.entryDate.format(DateTimeFormatter.ofPattern("EEE, d MMM")),
+                style = MaterialTheme.typography.caption
+            )
         }
     }
 }
@@ -77,6 +131,6 @@ fun NoteScreen() {
 @Preview(showBackground = true)
 @Composable
 fun NoteScreenPreview() {
-    NoteScreen()
+    NoteScreen(notes = NotesDataSource().loadNotes(), onAddNote = {}, onRemoveNote = {})
 }
 
